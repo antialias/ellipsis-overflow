137cac541a6a11d99fd536239d5c114b413f99f2
Thomas Hallock <thomas@1stdibs.com>
2016-01-22 15:17:37 -0500

    converted from jqueyr plugin to commonjs module

--- ellipsis.js
+++ ellipsis.js
@@ -1,146 +1,106 @@
-// https://github.com/antialias/ellipsis.jquery.js
-(function (root, factory) {
-	"use strict";
-	var m = {};
-	if ("object" === typeof root.module) {
-		factory(
-			module,
-			require('jquery')
-		);
-	} else {
-		factory(
-			m,
-			window.jQuery
-		);
+"use strict";
+var assign = require('lodash.assign');
+var contentKey = '__ellipsis-original-content';
+var canscroll = function (el, tolerance) {
+	var scrollRoom = 0,
+		ost = 0, // old scroll top
+		nst, // new scroll top
+		origScrolltop;
+	// Determine if $el can be scrolled by modifying scrollTop and checking if the change has stuck
+	// Note: browsers have different tolerances.
+	origScrolltop = el.scrollTop;
+	el.scrollTop = 0;
+	el.scrollTop = 1;
+	nst = el.scrollTop; // new scroll top
+	while (ost !== nst) {
+		ost = el.scrollTop;
+		el.scrollTop = ost + 1;
+		nst = el.scrollTop;
+		++scrollRoom;
+		if (scrollRoom > tolerance) { // break early if we can
+			break;
+		}
 	}
-})(window, function (module, $) {
-	"use strict";
-// ellipsis
-// re-fills in an element with a set height (or max-height) so that there is no overflow.
-// If content was truncated, config.ellipsis is appended to the end in a way that does not cause further overflow.
-// 
-// usage: jQuery(element).ellipsis();
-// 
-	module.exports = jQuery;
-	$.fn.ellipsis = function (config) {
-		var finishedClamping = $.Deferred();
-		config = $.extend({
-			async: true,
-			skip_slow_browsers: false,
-			tolerance: 1, // maximum amount the element can scroll before triggering the truncation
-			content: false, // if not supplied here, content will be scraped from the element itself using $.fn.html()
-			ellipsis: " &hellip; " // will be concatenated to the end of the content if it is truncated
-		}, config);
-		var canscroll;
-		var doIt;
-		canscroll = function ($el) {
-			var scrollRoom = 0,
-				ost = 0, // old scroll top
-				nst, // new scroll top
-				origScrolltop;
-			// Determine if $el can be scrolled by modifying scrollTop and checking if the change has stuck
-			// Note: browsers have different tolerances.
-			origScrolltop = $el.scrollTop();
-			$el.scrollTop(0);
-			$el.scrollTop(1);
-			nst = $el.scrollTop(); // new scroll top
-			while (ost !== nst) {
-				ost = $el.scrollTop();
-				$el.scrollTop(ost + 1);
-				nst = $el.scrollTop();
-				++scrollRoom;
-				if (scrollRoom > config.tolerance) { // break early if we can
-					break;
-				}
-			}
-			$el.scrollTop(origScrolltop); // put it back so nobody suspects nothin'
-			return scrollRoom > config.tolerance;
-		};
-		if (config.skip_slow_browsers && $.browser.msie && $.browser.version < 8) {
-			return this;
-		}
-		doIt = $.proxy(function () {
-			this.map($.proxy(function () {
-				var contentKey = '__ellipsis-original-content';
-				var low = 0;
-				var high;
-				var test;
-				var try_this;
-				var truncated;
-				var ellipsis;
-				var after_ellipsis;
-				var el;
-				var breakables;
-				var content;
-				content = config.content;
-				if (false === content) {
-					content = $(this).data(contentKey);
-					if (undefined === content) {
-						content = $(this).html();
-						$(this).data(contentKey, content);
-					} else {
-						$(this).html(content);
-					}
-				}
-				if (!content) {
-					return;
-				}
-				breakables = content.split(/\s/);
-				el = $(this);
-				if (!canscroll(el)) {
-					return;
-				}
-				$(this).html("");
-				breakables.push(" ");
-				breakables.push(" ");
-				low = 0;
-				high = $(breakables).length;
-				while (high > low + 1) { // binary search to find the scrolling point
-					test = Math.floor((low + high) / 2);
-					el.html(breakables.slice(0, test).join(" ") + " ");
-					if (canscroll(el)) {
-						high = test;
-					} else {
-						low = test;
-					}
-				}
-				var last = breakables.slice(0, high - 1).join(' ') + " ";
-				after_ellipsis = $("<span>")
-					.css('display', 'none')
-					.addClass('ellipsised-content');
-				ellipsis = $("<span>")
-					.addClass('ellipsis')
-					.html(config.ellipsis);
-				truncated = $("<span>")
-					.addClass('truncated-content')
-					.html(last);
-				el
-					.html("")
-					.append(truncated)
-					.append(ellipsis)
-					.append(after_ellipsis);
-				try_this = last;
-				// if the ellipsis has made the element scroll again, back off character by character until it doesnt
-				while (try_this) {
-					if (try_this.substr(-1).match(/\s/) && !canscroll(el)) { // if we are at a space and cannot scroll
-						break;
-					}
-					try_this = try_this.substr(0, try_this.length - 1);
-					truncated.html(try_this);
-				}
-				if (try_this.length !== last.length) {
-					after_ellipsis.html(last.substr(try_this.length - last.length));
-				}
-				after_ellipsis.html(after_ellipsis.html() + breakables.slice(high - 1).join(" "));
-				el.html($.trim(el.html()));
-			}, this));
-			finishedClamping.resolve();
-		}, this);
-		if (config.async) {
-			setTimeout(doIt);
+	el.scrollTop = origScrolltop; // put it back so nobody suspects nothin'
+	return scrollRoom > tolerance;
+};
+module.exports = function (el, config) {
+    config = config || {};
+    var _canscroll;
+	var low = 0;
+	var high;
+	var test;
+	var try_this;
+	var truncated;
+	var ellipsis;
+	var after_ellipsis;
+	var breakables;
+	var content;
+	config = assign({
+		async: true,
+		skip_slow_browsers: false,
+		tolerance: 1, // maximum amount the element can scroll before triggering the truncation
+		content: false, // if not supplied here, content will be scraped from the element itself using $.fn.html()
+		ellipsis: " &hellip; " // will be concatenated to the end of the content if it is truncated
+	}, config);
+    _canscroll = canscroll.bind(null, el, config.tolerance);
+	content = config.content;
+	if (false === content) {
+		content = el[contentKey];
+		if (undefined === content) {
+			content = el.innerHTML;
+			el[content] = content;
 		} else {
-			doIt();
+			el.innerHTML = content;
 		}
-		return finishedClamping;
-	};
-});
+	}
+	if (!content) {
+		return;
+	}
+	breakables = content.split(/\s/);
+	if (!_canscroll()) {
+		return;
+	}
+	el.innerHTML = "";
+	breakables.push(" ");
+	breakables.push(" ");
+	low = 0;
+	high = breakables.length;
+	while (high > low + 1) { // binary search to find the scrolling point
+		test = Math.floor((low + high) / 2);
+		el.innerHTML = breakables.slice(0, test).join(" ") + " ";
+		if (_canscroll()) {
+			high = test;
+		} else {
+			low = test;
+		}
+	}
+	var last = breakables.slice(0, high - 1).join(' ') + " ";
+    after_ellipsis = document.createElement('span');
+    after_ellipsis.style.display = 'none';
+    after_ellipsis.classList.add('ellipsised-content');
+    ellipsis = document.createElement('span');
+    ellipsis.classList.add('ellipsis');
+    ellipsis.innerHTML = config.ellipsis;
+    truncated = document.createElement('span');
+    truncated.classList.add('truncated-content');
+    truncated.innerHTML = last;
+    el.innerHTML = '';
+    el.appendChild(truncated);
+    el.appendChild(ellipsis);
+    el.appendChild(after_ellipsis);
+	try_this = last;
+	// if the ellipsis has made the element scroll again, back off character by character until it doesnt
+	while (try_this) {
+		if (try_this.substr(-1).match(/\s/) && !_canscroll()) { // if we are at a space and cannot scroll
+			break;
+		}
+		try_this = try_this.substr(0, try_this.length - 1);
+		truncated.innerHTML = try_this;
+	}
+	if (try_this.length !== last.length) {
+		after_ellipsis.innerHTML = last.substr(try_this.length - last.length);
+	}
+	after_ellipsis.innerHTML += breakables.slice(high - 1).join(" ");
+	el.innerHTML = el.innerHTML.trim();
+}
---
+++ test/test.js
@@ -0,0 +1,42 @@
+var ellipsis = require('../ellipsis');
+var assert = require('node-assertthat');
+var scaffoldingTemplate = require('html!./scaffolding.html');
+var style = require('style/useable!css!./style.css');
+describe('ellipsis-overflow', function () {
+	var oneLine;
+	var happyPath;
+	beforeEach(function () {
+		document.body.innerHTML = scaffoldingTemplate;
+		style.use();
+		oneLine = document.getElementById("one-line");
+		happyPath = document.getElementById("happy-path");
+	});
+	afterEach(function () {
+		style.unuse();
+	})
+	it("content can be tested", function () {
+		assert(happyPath, "test element exists");
+		assert(happyPath.offsetHeight > 0, "test element has a height");
+		assert.that(happyPath.scrollHeight, is.greaterThan(happyPath.offsetHeight), "test element is scrollable");
+	});
+	it("reduces scrollHeight", function () {
+		var beforeScrollHeight = happyPath.scrollHeight;
+		ellipsis(happyPath);
+		assert.that(beforeScrollHeight, is.greaterThan(happyPath.scrollHeight));
+	});
+	it("test one-liner", function () {
+		ellipsis(oneLine);
+		// "scrollHeight is less than offsetHeight"
+		assert.that(oneLine.scrollHeight, is.lessThan(oneLine.offsetHeight));
+	});
+	it("test zero height", function () {
+		var zeroHeight = document.getElementById("zero-height");
+		ellipsis(zeroHeight);
+		assert.that(oneLine.offsetHeight, is.greaterThan(zeroHeight.scrollHeight), "zero height element scroll height is less than a line");
+	});
+	it("what happens when overflow:visible is set", function () {
+		var noOverflow = document.getElementById("no-overflow");
+		ellipsis(noOverflow);
+		assert.that(noOverflow.offsetHeight, is.lessThan(noOverflow.scrollHeight), "scroll height is greater than offset height, because we don't have overflow:scroll");
+	});
+});
